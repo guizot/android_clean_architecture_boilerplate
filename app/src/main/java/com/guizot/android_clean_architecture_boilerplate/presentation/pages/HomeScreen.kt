1bc5dd9b2e8b57c37c0a3b822714d3384c5641c9
Guizot <guizot@outlook.com>
2025-02-09 20:00:55 +0700

    remove unused import

--- app/src/main/java/com/guizot/android_clean_architecture_boilerplate/presentation/pages/HomeScreen.kt
+++ app/src/main/java/com/guizot/android_clean_architecture_boilerplate/presentation/pages/HomeScreen.kt
@@ -3,20 +3,10 @@ package com.guizot.android_clean_architecture_boilerplate.presentation.pages
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavHostController
-import com.guizot.android_clean_architecture_boilerplate.presentation.core.widget.CustomAppBar
 
 @Composable
 fun HomeScreen() {
--- app/src/main/java/com/guizot/android_clean_architecture_boilerplate/presentation/pages/SettingScreen.kt
+++ app/src/main/java/com/guizot/android_clean_architecture_boilerplate/presentation/pages/SettingScreen.kt
@@ -1,23 +1,18 @@
 package com.guizot.android_clean_architecture_boilerplate.presentation.pages
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.guizot.android_clean_architecture_boilerplate.domain.model.ChipItem
 import com.guizot.android_clean_architecture_boilerplate.domain.model.CommonItemModel
 import com.guizot.android_clean_architecture_boilerplate.presentation.core.widget.ChipGroup
 import com.guizot.android_clean_architecture_boilerplate.presentation.core.widget.CommonItem
-import com.guizot.android_clean_architecture_boilerplate.presentation.core.widget.CustomAppBar
 
 @Composable
 fun SettingScreen() {
