6514a5236f070f5740c9a8178ca0c2116452789b
autentika <rinaldi@autentika.id>
2025-02-10 16:44:16 +0700

    move store api key

--- app/src/main/java/com/guizot/android_clean_architecture_boilerplate/data/source/remote/interceptor/GithubInterceptor.kt
+++ app/src/main/java/com/guizot/android_clean_architecture_boilerplate/data/source/remote/interceptor/GithubInterceptor.kt
@@ -1,5 +1,6 @@
 package com.guizot.android_clean_architecture_boilerplate.data.source.remote.interceptor
 
+import com.guizot.android_clean_architecture_boilerplate.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -7,7 +8,7 @@ import javax.inject.Inject
 class GithubInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "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") // Replace with actual token
+            .addHeader("Authorization", BuildConfig.GITHUB_API_KEY)
             .build()
         return chain.proceed(request)
     }
